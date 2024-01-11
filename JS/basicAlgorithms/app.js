693c4ab8aa36d1824dbb29224d23626a73ee4e11
NachoGracia <gracianacho@gmail.com>
2024-01-11 13:30:07 +0100

    templateStringEn#1.2

--- JS/basicAlgorithms/app.js
+++ JS/basicAlgorithms/app.js
@@ -71,7 +71,7 @@ firstName = 'Jon';
 	lastName = 'Snow'; 
 	age = 24;
 
-    console.log("Soy " + firstName + " " + lastName + ", " + "tengo " + age + " años y me gustan los lobos");
+    console.log(`Soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos `);
 
 //? 1.3 --------------------------------------------------
 
